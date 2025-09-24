5e276fb9dff0c79906085c8c76bf3feb9234b4bb
Andy Briggs <andrew@brggs.co.uk>
2025-09-24 21:51:05 +0100

    Styling updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import { visit } from 'unist-util-visit';
+
+// Custom remark plugin to handle external links
+function remarkExternalLinks() {
+  return (tree) => { 
+    visit(tree, 'link', (node) => {
+      if (/^(https?):\/\/[^\s/$.?#].[^\s]*$/i.test(node.url) && !node.url.includes('brggs.co.uk')) {
+        node.data = node.data || {};
+        node.data.hProperties = node.data.hProperties || {};
+        node.data.hProperties.target = '_blank';
+        node.data.hProperties.rel = 'noopener noreferrer';
+      }
+    });
+  };
+}
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  markdown: {
+    remarkPlugins: [remarkExternalLinks],
+  }
 });
